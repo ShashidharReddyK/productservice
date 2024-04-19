af8f6744a886b464ecc24f6d9a5e7c7a9b419f72
Shashidhar Reddy <reddykshashidhar4@gmail.com>
2024-04-19 10:48:17 +0530

    Added latest changes

--- src/test/java/com/example/productservice/ProductserviceApplicationTests.java
+++ src/test/java/com/example/productservice/ProductserviceApplicationTests.java
@@ -18,29 +18,29 @@ class ProductserviceApplicationTests {
     @Autowired
     private SelfProductService selfProductService;
 
-//    @Test
-//    void contextLoads() {
-//    }
-//
-//
-//
-//
     @Test
-    void testQueries(){
-        Product product = new Product();
-        product.setTitle("Iphone 15 pro");
-        Category category = new Category();
-        category.setName("Iphone");
-////
-
-        product.setDescription("Latest Iphone  15 pro");
-        product.setPrice(149000.00);
-        product.setCategory(category);
-
-//    productRepository.save(product);
-//         productRepository.findById(1L);
-        selfProductService.patchProduct(11L,product);
-
-        assertEquals(1,1);
+    void contextLoads() {
     }
+//
+//
+//
+//
+//    @Test
+//    void testQueries(){
+//        Product product = new Product();
+//        product.setTitle("Iphone 15 pro");
+//        Category category = new Category();
+//        category.setName("Iphone");
+//////
+//
+//        product.setDescription("Latest Iphone  15 pro");
+//        product.setPrice(149000.00);
+//        product.setCategory(category);
+//
+////    productRepository.save(product);
+////         productRepository.findById(1L);
+//        selfProductService.patchProduct(11L,product);
+//
+//        assertEquals(1,1);
+//    }
 }
